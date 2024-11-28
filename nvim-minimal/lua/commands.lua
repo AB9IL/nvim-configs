7e604064714d12bf2654eab5e888f2097cbbdc5b
Philip Colllier <redcrown01@gmail.com>
2024-11-28 01:31:15 +0000

    Update commands.lua

--- nvim-minimal/lua/commands.lua
+++ nvim-minimal/lua/commands.lua
@@ -3,35 +3,66 @@ local augroup = vim.api.nvim_create_augroup
 -- Create autocommand
 local autocmd = vim.api.nvim_create_autocmd
 -- remove trailing whitespaces
-autocmd('BufWritePre', {
-  pattern = '*',
-  command = ":%s/\\s\\+$//e"
+autocmd("BufWritePre", {
+	pattern = "*",
+	command = ":%s/\\s\\+$//e",
 })
 -- Run xrdb whenever Xdefaults or Xresources are updated.
-autocmd('BufWritePost', {
-  pattern = 'X{resources,defaults}',
-  command = ":silent !xrdb %"
+autocmd("BufWritePost", {
+	pattern = "X{resources,defaults}",
+	command = ":silent !xrdb %",
 })
 -- Update binds when sxhkdrc is updated.
-autocmd('BufWritePost', {
-  pattern = '*sxhkdrc',
-  command = ":!pkill -USR1 sxhkd"
+autocmd("BufWritePost", {
+	pattern = "*sxhkdrc",
+	command = ":!pkill -USR1 sxhkd",
 })
 -- Prevent resize glitch on open
-autocmd('VimEnter', {
-    pattern = '*',
-    command = ":silent !kill -s SIGWINCH $PPID"
+autocmd("VimEnter", {
+	pattern = "*",
+	command = ":silent !kill -s SIGWINCH $PPID",
 })
 -- Set filetype syntax and behavior
-autocmd({"BufNewFile", "BufRead"}, {pattern = {"*.markdown", "*.mdown", "*.mkd", "*.mdwn", "*.md", "*-outline", "*-manuscript"}, command = "set ft=markdown wrap linebreak nolist"})
-autocmd({"BufNewFile", "BufRead"}, {pattern = {"*.txt", "*.text", "*.log"}, command = "set ft=text wrap linebreak nolist"})
-autocmd({"BufNewFile", "BufRead"}, {pattern = {"conf", "config", "*.conf", "*rc", "*.rc", "*.strm", "*.xspf"}, command = "set ft=config"})
-autocmd({"BufNewFile", "BufRead"}, {pattern = {"*.wiki"}, command = "set ft=vimwiki"})
+autocmd({ "BufNewFile", "BufRead" }, {
+	pattern = {
+		"*.markdown",
+		"*.mdown",
+		"*.mkd",
+		"*.mdwn",
+		"*.md",
+		"*-outline",
+		"*-manuscript",
+	},
+	command = "set ft=markdown wrap linebreak nolist",
+})
+autocmd({ "BufNewFile", "BufRead" }, {
+	pattern = {
+		"*.txt",
+		"*.text",
+	},
+	command = "set ft=text wrap linebreak nolist",
+})
+autocmd({ "BufNewFile", "BufRead" }, {
+	pattern = {
+		"conf",
+		"config",
+		"*.conf",
+		"*rc",
+		"*.rc",
+		"*.strm",
+		"*.xspf",
+		"*.log",
+	},
+	command = "set ft=config",
+})
 -- Highlight on yank
-augroup('YankHighlight', { clear = true })
-autocmd('TextYankPost', {
-  group = 'YankHighlight',
-  callback = function()
-    vim.highlight.on_yank({ higroup = (vim.fn['hlexists']('HighlightedyankRegion') > 0 and 'HighlightedyankRegion' or 'IncSearch'), timeout = '100' })
-  end
+augroup("YankHighlight", { clear = true })
+autocmd("TextYankPost", {
+	group = "YankHighlight",
+	callback = function()
+		vim.highlight.on_yank({
+			higroup = (vim.fn["hlexists"]("HighlightedyankRegion") > 0 and "HighlightedyankRegion" or "IncSearch"),
+			timeout = "100",
+		})
+	end,
 })
