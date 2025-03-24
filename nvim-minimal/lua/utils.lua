7c4c9f28a083021aed6d93d00795005927fa44cb
Philip Collier <redcrown01@gmail.com>
2025-03-24 18:18:49 +0000

    My comments about the latest changes.

--- nvim-minimal/lua/utils.lua
+++ nvim-minimal/lua/utils.lua
@@ -1,66 +1,29 @@
 -- status line
-vim.cmd([[
-au VimEnter * hi statusline guifg=#CCCCDD guibg=#262626 ctermfg=White ctermbg=Black
-function! InsertStatuslineColor(mode)
-    if a:mode == 'i'
-        hi statusline guifg=#5080DD guibg=#262626 ctermfg=Cyan ctermbg=Black
-    elseif a:mode == 'r'
-        hi statusline  guifg=#DD4040 guibg=#262626 ctermfg=Red  ctermbg=Black
-    else
-        hi statusline guifg=#CCCCDD guibg=#262626 ctermfg=White ctermbg=Black
-    endif
-endfunction
-au InsertEnter * call InsertStatuslineColor(v:insertmode)
-au InsertLeave * hi statusline guifg=#CCCCDD guibg=#262626 ctermfg=White ctermbg=Black
-]])
+-- Clear the statusline for when the configuration is reloaded
+vim.o.statusline = ""
 
-local mode_map = {
-	['n'] = 'NORMAL ',
-	['no'] = 'NORMAL·OPERATOR PENDING ',
-	['v'] = 'VISUAL ',
-	['V'] = 'V·LINE ',
-	[''] = 'V·BLOCK ',
-	['s'] = 'SELECT ',
-	['S'] = 'S·LINE ',
-	[''] = 'S·BLOCK ',
-	['i'] = 'INSERT ',
-	['R'] = 'REPLACE ',
-	['Rv'] = 'V·REPLACE ',
-	['c'] = 'COMMAND ',
-	['cv'] = 'VIM EX ',
-	['ce'] = 'EX ',
-	['r'] = 'PROMPT ',
-	['rm'] = 'MORE ',
-	['r?'] = 'CONFIRM ',
-	['!'] = 'SHELL ',
-	['t'] = 'TERMINAL ',
-}
+-- Define status bar colors
+vim.cmd("hi! NormalColor guifg=#50DD6A guibg=#262626 ctermfg=Grey ctermbg=Black")
+vim.cmd("hi! InsertColor guifg=#5080DD guibg=#262626 ctermfg=Blue ctermbg=Black")
+vim.cmd("hi! ReplaceColor guifg=#DD4040 guibg=#262626 ctermfg=Red ctermbg=Black")
+vim.cmd("hi! VisualColor guifg=#DD40DD guibg=#262626 ctermfg=Magenta ctermbg=Black")
+vim.cmd("hi! CommandColor guifg=#CCCCDD guibg=#262626 ctermfg=Magenta ctermbg=Black")
 
-function mode()
-	local m = vim.api.nvim_get_mode().mode
-	if mode_map[m] == nil then return m end
-	return mode_map[m]
-end
-
-function status_line()
-  local stl = {
-  '%#PrimaryBlock#',
-  mode(),
-    '%3n ',
-    '%#SecondaryBlock#',
-    '%#Blanks#',
-    '%f ',
-    '%h%m%r%w ',
-    '%=',
-    '%{&fileformat} ',
-    '| %{&fenc} ',
-    '| %{&filetype} ',
-    '%#SecondaryBlock#',
-    ' %<%p%% ',
-    '%-4.(%l:%c%)',
-    '%#PrimaryBlock#',
-  }
-  return table.concat(stl)
-end
-
-vim.opt.statusline = "%{%v:lua.status_line()%}"
+-- Set the statusline
+vim.o.statusline = table.concat({
+    "%#NormalColor#%{mode()=='n'?' NORMAL ':''}",
+    "%#InsertColor#%{mode()=='i'?' INSERT ':''}",
+    "%#ReplaceColor#%{mode()=='R'?' REPLACE ':''}",
+    "%#VisualColor#%{mode()=='v'?' VISUAL ':''}",
+    "%#VisualColor#%{mode()==''?' VISUAL ':''}",
+    "%#CommandColor#%{mode()=='c'?' COMMAND ':''}",
+    "%3n",  -- buffer number
+    " %f",  -- file name
+    " %h%m%r%w",  -- flags
+    " %= ",  -- right align
+    "%{&fileformat} | ",  -- file format
+    "%{strlen(&fenc)?&fenc:&enc} | ",  -- encoding
+    "%{strlen(&ft)?&ft:'none'} ",  -- filetype
+    "%<%p%% ",  -- position
+    "%-4.(%l:%c%) ",  -- offset
+}, "")
