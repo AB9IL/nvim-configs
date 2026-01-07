c2ff78bd8f8570d402f675b2bf50e24698340c65
Philip Collier <redcrown01@gmail.com>
2026-01-07 00:58:30 +0000

    Mass push without comment.

--- nvim-minimal/lua/mappings.lua
+++ nvim-minimal/lua/mappings.lua
@@ -118,7 +118,7 @@ keymap('n', '<C-w>t', ':terminal<CR>', opts)
 keymap('n', ',q', ':q!<CR>', opts)
 
 -- Automatically close brackets, parethesis, and quotes
-keymap('i', "'", "''<left>", opts)
+keymap('i', " '", " ''<left>", opts)
 keymap('i', '"', '""<left>', opts)
 keymap('i', '(', '()<left>', opts)
 keymap('i', '[', '[]<left>', opts)
